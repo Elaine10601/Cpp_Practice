05090058caddc332c70f34ca04cbe57cd6691784
Elaine10601 <93778764+Elaine10601@users.noreply.github.com>
2021-12-02 09:29:11 +0800

    Update 06_將輸入順序反向.cpp

--- 06_將輸入順序反向.cpp
+++ 06_將輸入順序反向.cpp
@@ -19,3 +19,20 @@ int main()
 	}
 	return 0;
 }
+
+
+
+//字串物件
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string str1;
+	getline(cin,str1);
+	for (int i=str1.length()-1; i>=0; i--)
+	{
+		cout << str1[i];
+	}
+	return 0;
+}
