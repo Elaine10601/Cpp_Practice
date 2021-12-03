7a638395b3809349506afc5b206ec35cf6a75ec7
Elaine10601 <93778764+Elaine10601@users.noreply.github.com>
2021-12-03 20:19:52 +0800

    Create 5-1.cpp

---
+++ Code_Example/CH5/5-1.cpp
@@ -0,0 +1,16 @@
+//filename :5-1
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "�п�J�@����ơG";
+    cin >> num;
+    cout << "|" << num << "| = ";
+    if (num<0)
+       num=-num;
+    cout << num << endl;
+
+ 	return 0;
+}
+
