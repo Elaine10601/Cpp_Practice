63dac8ab6e3cdcd0e3386f79f64e43ea03744639
Elaine10601 <93778764+Elaine10601@users.noreply.github.com>
2021-12-03 20:16:37 +0800

    Add files via upload

---
+++ Code_Example/CH1/debug.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std;
+int main()
+{
+  int r;
+  float A;
+  const float pi=3.1416;  //pi為常數，資料型態是浮點數，內容為3.1416 
+  r=5
+  A=r*r*pi;
+  cout <<"圓形半徑="<<r<<endl;
+  cout<<"圓形面積 =<<A<<endl;
+}
---
+++ Code_Example/CH1/wonderfulDay.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "It's a wonderful day!";
+	return 0;
+ }
