432a5efa72ca84d21832434710b6128047c682d8
Elaine10601 <93778764+Elaine10601@users.noreply.github.com>
2021-12-10 19:55:46 +0800

    Add files via upload

---
+++ ColorChange.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <windows.h>
+using namespace std;
+int main()
+{	
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (hOut == INVALID_HANDLE_VALUE)return GetLastError();
+	DWORD dwMode = 0;
+	if (!GetConsoleMode(hOut, &dwMode))return GetLastError();
+	dwMode |= 0x0004;
+	if (!SetConsoleMode(hOut, dwMode))return GetLastError();
+	int r=100,g=100,b=100;
+	wprintf(L"\x1b[38;2;%d;%d;%dmhello,world!",r,g,b);
+	return 0;
+}
+
